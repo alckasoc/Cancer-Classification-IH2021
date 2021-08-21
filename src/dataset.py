4dcdb74ff06b0372276eecc21026e35018870fc1
Furkan Kunt <kamilfurkankunt@gmail.com>
2021-08-21 17:11:24 +0300

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -19,36 +19,6 @@ class SkinCancerDatasetRetriever(torch.utils.data.Dataset):
 		assert mode in ['train', 'valid']
 		self.mode = mode
 
-		if self.mode == 'train':
-			self.transform = albu.Compose([
-				albu.RandomResizedCrop(height=self.image_size, width=self.image_size, scale=(0.25, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=1, p=1.0),
-				albu.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=30, interpolation=1, border_mode=0, value=0, p=0.25),
-				albu.HorizontalFlip(p=0.5),
-				albu.VerticalFlip(p=0.5),
-				albu.OneOf([
-					albu.MotionBlur(p=.2),
-					albu.MedianBlur(blur_limit=3, p=0.1),
-					albu.Blur(blur_limit=3, p=0.1),
-				], p=0.25),
-				albu.OneOf([
-					albu.CLAHE(clip_limit=2),
-					albu.IAASharpen(),
-					albu.IAAEmboss(),
-					albu.RandomBrightnessContrast(),            
-				], p=0.25),
-				albu.Cutout(num_holes=4, max_h_size=32, max_w_size=32, fill_value=0, p=0.25),
-				albu.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
-				ToTensorV2(),
-			])
-		
-		# infer or validation data
-		else:
-			self.transform = albu.Compose([
-				albu.Resize(self.image_size, self.image_size),
-				albu.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
-				ToTensorV2(),
-			])
-
 	def __len__(self):
 		return len(self.df)
 
@@ -62,8 +32,8 @@ class SkinCancerDatasetRetriever(torch.utils.data.Dataset):
 
 		label = torch.FloatTensor(self.df.loc[index, classes])
 
-		transformed = self.transform(image=image)
-		image = transformed["image"]
+		#transformed = self.transform(image=image)
+		#image = transformed["image"]
 		
 		
 		if self.mode == 'train':
